2a9c3ce2e577eb245d58a20368b6ecbc7e215a8f
GhanshyamKhadka <160210230+GhanshyamKhadka@users.noreply.github.com>
2025-01-16 17:02:23 +0545

    Delete Portfolio(.py) directory

--- Portfolio(.py)/WEEK-06/program1.py
+++
@@ -1,9 +0,0 @@
-''' Write a function that accepts a positive integer as a parameter and then returns a
- representation of that number in binary (base 2).
- Hint: This is in many ways a trick question. Think!'''
-
-def binary(n):
-    return bin(n)[2:]
-
-
-print(binary(10))  
